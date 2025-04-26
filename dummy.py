10a36d636552b605c3e14a25ea9fa98ee9f2e059
GenAIUnplugged <deeplearningunplugged@gmail.com>
2025-04-27 00:46:54 +0530

    Adding Project files

--- dummy.py
+++
@@ -1,7 +0,0 @@
-import os
-from datetime import datetime
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-current_dir = os.path.dirname(os.path.abspath(__file__))
-filename = os.path.join(current_dir, f"claude_chat_{timestamp}.txt")
-filename = filename.replace("\\", "\\\\")
-print(filename)
